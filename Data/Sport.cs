fe500f0f8db708388c54e37b870da9bb0589ce85
Carlos Hernández <Calichi15@hotmail.com>
2024-03-21 15:17:54 -0500

    Adding EF Core 2

---
+++ Data/Sport.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dxt.Data;
+
+public class Sport(DbContextOptions<Sport> options) : DbContext(options)
+{
+    public DbSet<Models.Player> Players { get; set; }
+}
