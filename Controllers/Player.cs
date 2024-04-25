9d3d0a1fd4925a542fae2b0505dacdd6c6e2b9ee
Carlos Hernández <Calichi15@hotmail.com>
2024-04-25 16:19:50 -0500

    prueba one

--- Controllers/Player.cs
+++ Controllers/Player.cs
@@ -30,30 +30,31 @@ public class Player(Services.Player dtoPlayer, BlobServiceClient _blob) : Contro
     [Authorize]
     [RequiredScope("Players.Write.All")]
     public async Task<IActionResult> Create(IFormFile image, [FromForm]Model.Player model) {
-        model.Id = User.GetObjectId()!;
-        if(await dtoPlayer.Contains(model.Id))
-            return Conflict("¡Esta cuenta ya esta registrada!");
+        // model.Id = User.GetObjectId()!;
+        // if(await dtoPlayer.Contains(model.Id))
+        //     return Conflict("¡Esta cuenta ya esta registrada!");
 
-        bool isThereNotContainerYet = true;
-        await foreach(var item in _blob.GetBlobContainersAsync()) {
-            if(item.Name == model.Id) {
-                isThereNotContainerYet = false;
-                break;
-            }
-        };
+        // bool isThereNotContainerYet = true;
+        // await foreach(var item in _blob.GetBlobContainersAsync()) {
+        //     if(item.Name == model.Id) {
+        //         isThereNotContainerYet = false;
+        //         break;
+        //     }
+        // };
 
-        if(isThereNotContainerYet) {
-            BlobContainerClient container = await _blob.CreateBlobContainerAsync(model.Id);
+        // if(isThereNotContainerYet) {
+        //     BlobContainerClient container = await _blob.CreateBlobContainerAsync(model.Id);
 
-            if(await container.ExistsAsync()) {
-                var blob = container.GetBlobClient(image.FileName);
-                await blob.UploadAsync(image.OpenReadStream(), true);
-                model.ImageProfile = blob.Uri.ToString();
-            }
-        }
+        //     if(await container.ExistsAsync()) {
+        //         var blob = container.GetBlobClient(image.FileName);
+        //         await blob.UploadAsync(image.OpenReadStream(), true);
+        //         model.ImageProfile = blob.Uri.ToString();
+        //     }
+        // }
 
-        await dtoPlayer.Add(model);
-        return CreatedAtAction(nameof(Get), new {id = model.Id}, model);
+        // await dtoPlayer.Add(model);
+        //return CreatedAtAction(nameof(Get), new {id = model.Id}, model);
+        return Ok();
     }
 
     [HttpPut("{id}")]
