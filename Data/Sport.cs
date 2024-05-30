b28cac4a727ff15d38dfc28ead262d34f38144e3
Carlos Hernández <Calichi15@hotmail.com>
2024-05-30 15:12:04 -0500

    Including DbContext

---
+++ Data/Sport.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dxt.Data;
+
+public class Sport(DbContextOptions<Sport> options) : DbContext(options)
+{
+    public DbSet<Model.Player> Players { get; set; }
+    public DbSet<Model.Team> Teams { get; set; }
+    public DbSet<Model.Person> Persons { get; set; }
+
+}
