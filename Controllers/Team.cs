98d7d4bcfc71276943092c687b285f9c65da658f
Carlos Hernández <Calichi15@hotmail.com>
2024-05-30 18:08:35 -0500

    New Endpoint

---
+++ Controllers/Team.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dxt.Controller;
+
+[ApiController]
+[Route("[controller]")]
+public class Team(Service.Team teams) : ControllerBase
+{
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> RegisterAsync(Model.Team team)
+    {
+        if ( await teams.ContainsAsync( team ) )
+            return Conflict("¡Ya existe un equipo registrado con este nombre!");
+
+        await teams.AddAsync( team );
+        return CreatedAtAction(nameof(GetAsync), new {id = team.Id}, team);
+    }
+
+    [HttpGet("{id}")]
+    [Authorize]
+    public async Task<ActionResult<Model.Team>> GetAsync(long id)
+    {
+        if ( await teams.GetAsync(id) is Model.Team team )
+            return team;
+            
+        return NotFound();
+    }
+
+}
---
+++ Services/Team.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace dxt.Service;
+
+public class Team(Database.Context db)
+{
+    public async Task AddAsync(Model.Team team)
+    {
+        await db.AddAsync( team );
+        await db.SaveChangesAsync();
+    }
+
+    public async Task<bool> ContainsAsync(Model.Team team) =>
+        await db.Teams.AnyAsync( entry => entry.SearchId == team.SearchId );
+
+    public async Task<Model.Team?> GetAsync(long id) =>
+        await db.Teams.FindAsync( id );
+}
