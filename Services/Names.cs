f6b5c48a029e1c2321517e62142036b252bd3cd9
Carlos Hernández <Calichi15@hotmail.com>
2024-06-06 16:09:56 -0500

    New Service Names

---
+++ Services/Names.cs
@@ -0,0 +1,15 @@
+﻿namespace dxt;
+
+public class Names(Database.Context db)
+{
+    public ulong Add(string pName)
+    {
+        var name = Get(pName) ?? new() { Name = pName };
+        name.Records++;
+        db.SaveChanges();
+        return name.Records;
+    }
+
+    public Model.Person? Get(string name) =>
+        db.Persons.FirstOrDefault( p => p.Name == name );
+}
