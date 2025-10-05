13f6d7ce7de9d6ebe0f4d1fde8fd8a551618d1f7
Carlos Hernández Alarcón <carlosappdev@outlook.es>
2025-10-04 21:46:22 -0600

    Define relationships of TeamAffiliationRequest

--- Data/Sport.cs
+++ Data/Sport.cs
@@ -8,4 +8,18 @@ public class Context(DbContextOptions<Context> options) : DbContext(options)
     public DbSet<Model.Team> Teams { get; set; }
     public DbSet<Model.Person> Persons { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Model.TeamAffiliationRequest>()
+               .HasOne(e => e.Transmitter)
+               .WithMany(e => e.SentTeamAffiliationRequests)
+               .HasForeignKey("TransmitterId")
+               .IsRequired();
+
+        builder.Entity<Model.TeamAffiliationRequest>()
+               .HasOne(e => e.Receiver)
+               .WithMany(e => e.ReceivedTeamAffiliationRequests)
+               .HasForeignKey("ReceiverId")
+               .IsRequired();
+    }
 }
