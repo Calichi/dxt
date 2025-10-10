be53c9fe776c49c966fd272c86b6aee9b5818990
Carlos Hernández Alarcón <carlosappdev@outlook.es>
2025-10-10 17:45:02 -0600

    refactor: reset old changes

--- Controllers/Player.cs
+++ Controllers/Player.cs
@@ -9,67 +9,81 @@ namespace dxt.Controller;
 public class Player(Service.Player dtoPlayer,
                     Service.Names names) : ControllerBase
 {
-    [HttpGet]
-    [Authorize()]
-    [RequiredScope("Players.Read.All")]
-    public async Task<ActionResult<List<Model.Player>>> Get() =>
-        await dtoPlayer.GetAll();
+  [HttpGet]
+  [Authorize()]
+  [RequiredScope("Players.Read.All")]
+  public async Task<ActionResult<List<Model.Player>>> Get() =>
+      await dtoPlayer.GetAll();
 
-    [HttpGet("{id}")]
-    [Authorize]
-    [RequiredScope("Players.Read.All")]
-    public async Task<ActionResult<Model.Player>> Get(string id) {
-        if(await dtoPlayer.Get(id) is Model.Player player)
-            return player;
-        return NotFound();
-    }
-
-    [HttpPost]
-    [Authorize]
-    [RequiredScope("Players.Write.All")]
-    public async Task<IActionResult> Create(Model.Player player)
+  [HttpGet("{id}")]
+  [Authorize]
+  [RequiredScope("Players.Read.All")]
+  public async Task<ActionResult<Model.Player>> Get(string id)
+  {
+    try
     {
-        if(dtoPlayer.Contains(player.UniqueId))
-            return Conflict("¡Esta cuenta ya esta registrada!");
-
-        player.RecordNumber = names.Add(player.Name);
-        await dtoPlayer.Add(player.ResolveSearchId());
-        
-        return CreatedAtAction(nameof(Get), new {id = player.UniqueId}, player);
+      if (await dtoPlayer.Get(id) is Model.Player player)
+        return player;
+      return NotFound();
     }
-
-    [HttpPut("{id}")]
-    [Authorize]
-    [RequiredScope("Players.Update.Self")]
-    public IActionResult Update(string id, Model.Player player)
+    catch (Exception ex)
     {
-        if(id != player.UniqueId)
+        return BadRequest(new
         {
-            return BadRequest();
-        }
-        else if(dtoPlayer.Contains(player.UniqueId))
-        {
-            dtoPlayer.Update(player);
-            return NoContent();
-        }
-        else return NotFound();
-    }
+            message = ex.Message,
+            exceptionType = ex.GetType().FullName,
+            stackTrace = ex.StackTrace
+        });
 
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(string id)
-    {
-        if(dtoPlayer.GetByUniqueId(id) is Model.Player player)
-        {
-            try
-            {
-                await dtoPlayer.DeleteAsync(player);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest($"{ex}");
-            }
-            return NoContent();
-        }
-        else return NotFound();
     }
+  }
+
+  [HttpPost]
+  [Authorize]
+  [RequiredScope("Players.Write.All")]
+  public async Task<IActionResult> Create(Model.Player player)
+  {
+    if (dtoPlayer.Contains(player.UniqueId))
+      return Conflict("¡Esta cuenta ya esta registrada!");
+
+    player.RecordNumber = names.Add(player.Name);
+    await dtoPlayer.Add(player.ResolveSearchId());
+
+    return CreatedAtAction(nameof(Get), new { id = player.UniqueId }, player);
+  }
+
+  [HttpPut("{id}")]
+  [Authorize]
+  [RequiredScope("Players.Update.Self")]
+  public IActionResult Update(string id, Model.Player player)
+  {
+    if (id != player.UniqueId)
+    {
+      return BadRequest();
+    }
+    else if (dtoPlayer.Contains(player.UniqueId))
+    {
+      dtoPlayer.Update(player);
+      return NoContent();
+    }
+    else return NotFound();
+  }
+
+  [HttpDelete("{id}")]
+  public async Task<IActionResult> Delete(string id)
+  {
+    if (dtoPlayer.GetByUniqueId(id) is Model.Player player)
+    {
+      try
+      {
+        await dtoPlayer.DeleteAsync(player);
+      }
+      catch (Exception ex)
+      {
+        return BadRequest($"{ex}");
+      }
+      return NoContent();
+    }
+    else return NotFound();
+  }
 }
--- Services/Player.cs
+++ Services/Player.cs
@@ -4,78 +4,82 @@ namespace dxt.Service;
 
 public class Player(Database.Context sport)
 {
-    public async Task<List<Model.Player>> GetAll() => await sport.Players.ToListAsync();
+  public async Task<List<Model.Player>> GetAll() => await sport.Players.ToListAsync();
 
-    public async Task Add(Model.Player player)
+  public async Task Add(Model.Player player)
+  {
+    await sport.Players.AddAsync(player);
+    await sport.SaveChangesAsync();
+  }
+
+  public Model.Player? GetByUniqueId(string uniqueId) =>
+      sport.Players.FirstOrDefault(p => p.UniqueId == uniqueId);
+
+  public async Task<Model.Player?> Get(string uniqueId)
+  {
+    var player = await sport.Players
+        .Include(p => p.ReceivedTeamAffiliationRequests)
+        .Include(p => p.SentTeamAffiliationRequests)
+        .FirstOrDefaultAsync(p => p.UniqueId == uniqueId);
+
+    if (player is null) return null;
+
+    await sport.Entry(player)
+        .Collection(p => p.Teams)
+        .Query()
+        .Take(1)
+        .LoadAsync();
+    
+    return player;
+  }
+
+  public bool Contains(string uniqueId) => GetByUniqueId(uniqueId) is not null;
+
+  public void Update(Model.Player player)
+  {
+    sport.Players.Update(player);
+    sport.SaveChanges();
+  }
+
+  public async Task DeleteAsync(Model.Player player)
+  {
+    var strategy = sport.Database.CreateExecutionStrategy();
+    await strategy.ExecuteAsync(async () =>
     {
-        await sport.Players.AddAsync(player);
-        await sport.SaveChangesAsync();
-    }
+      await using var transaction = await sport.Database.BeginTransactionAsync();
+      try
+      {
+        // First delete player dependencies
+        await
+            sport.Teams
+                .Where(t => EF.Property<long>(t, "OwnerId") == player.Id)
+                .ExecuteDeleteAsync();
 
-    public Model.Player? GetByUniqueId(string uniqueId) =>
-        sport.Players.FirstOrDefault(p => p.UniqueId == uniqueId);
+        await
+            sport.Entry(player)
+                .Collection(e => e.SentTeamAffiliationRequests)
+                .Query()
+                .ExecuteDeleteAsync();
 
-    public async Task<Model.Player?> Get(string uniqueId)
-    {
-        if (GetByUniqueId(uniqueId) is Model.Player player)
-        {
-            await sport.Entry(player)
-            .Collection(p => p.Teams)
-            .Query()
-            .Take(1)
-            .LoadAsync();
-            return player;
-        }
-        return null;
-    }
+        await
+            sport.Entry(player)
+                .Collection(e => e.ReceivedTeamAffiliationRequests)
+                .Query()
+                .ExecuteDeleteAsync();
 
-    public bool Contains(string uniqueId) => GetByUniqueId(uniqueId) is not null;
+        // Then delete player
+        await
+            sport.Players
+                .Where(p => p.Id == player.Id)
+                .ExecuteDeleteAsync();
 
-    public void Update(Model.Player player)
-    {
-        sport.Players.Update(player);
-        sport.SaveChanges();
-    }
-
-    public async Task DeleteAsync(Model.Player player)
-    {
-        var strategy = sport.Database.CreateExecutionStrategy();
-        await strategy.ExecuteAsync(async () =>
-        {
-            await using var transaction = await sport.Database.BeginTransactionAsync();
-            try
-            {
-                // First delete player dependencies
-                await
-                sport.Teams
-                    .Where(t => EF.Property<long>(t, "OwnerId") == player.Id)
-                    .ExecuteDeleteAsync();
-
-                await
-                sport.Entry(player)
-                    .Collection(e => e.SentTeamAffiliationRequests)
-                    .Query()
-                    .ExecuteDeleteAsync();
-
-                await
-                sport.Entry(player)
-                    .Collection(e => e.ReceivedTeamAffiliationRequests)
-                    .Query()
-                    .ExecuteDeleteAsync();
-
-                // Then delete player
-                await
-                sport.Players
-                    .Where(p => p.Id == player.Id)
-                    .ExecuteDeleteAsync();
-
-                await transaction.CommitAsync();
-            }
-            catch
-            {
-                await transaction.RollbackAsync();
-                throw;
-            }
-        });
-    }
+        await transaction.CommitAsync();
+      }
+      catch
+      {
+        await transaction.RollbackAsync();
+        throw;
+      }
+    });
+  }
 }
